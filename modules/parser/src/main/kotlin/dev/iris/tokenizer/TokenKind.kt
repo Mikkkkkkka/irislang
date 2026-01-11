a3d2a0e66dbc22e846da6147929e9ae32c7d5eb0
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-01-11 15:45:04 +0300

    add TokenKind.kt

---
+++ modules/parser/src/main/kotlin/dev/iris/tokenizer/TokenKind.kt
@@ -0,0 +1,28 @@
+package dev.iris.tokenizer
+
+// Idea ругается на кириллицу в коде
+enum class TokenKind {
+    IDENT, INT_LITERAL,
+
+    KW_INT, KW_BOOL,                    // целч бул
+    KW_IF, KW_ELSE,                     // если иначе
+    KW_WHILE,                           // пока
+    KW_RETURN,                          // вернуть
+    KW_CONTINUE,                        // продолжить
+    KW_BREAK,                           // выйти
+    KW_PROCEDURE,                       // процедура
+    KW_FUNCTION,                        // функция
+    KW_STRUCT,                          // структура
+    KW_NEW,                             // новый
+    KW_PRINT,                           // печать
+    KW_START, KW_END,                   // нч кц
+    KW_TRUE, KW_FALSE,                  // правда ложь
+
+    PLUS, MINUS, STAR, SLASH, PERCENT,  // + - * / %
+    EQ, EQEQ, NEQ,                      // = == !=
+    LT, GT, GTEQ, LTEQ,                 // < > <= >=
+    DOT, COMMA, COLON, SEMICOLON,       // . , : ;
+    LPAREN, RPAREN,                     // ( )
+
+    EOF,
+}
