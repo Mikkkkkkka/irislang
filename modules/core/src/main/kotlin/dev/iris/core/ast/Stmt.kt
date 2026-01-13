210b8c0b4722790aac31f132fb384590ca23aaf1
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-01-13 18:53:36 +0300

    added iris statements

--- modules/core/src/main/kotlin/dev/iris/core/ast/Stmt.kt
+++ modules/core/src/main/kotlin/dev/iris/core/ast/Stmt.kt
@@ -3,11 +3,42 @@ package dev.iris.core.ast
 import dev.iris.core.Span
 
 sealed interface Stmt {
-    val span: Span?
+    val span: Span?     // Для сообщений об ошибках
+
+    data class Block(
+        val stmts: List<Stmt>,
+        override val span: Span? = null
+    ) : Stmt
 
     data class VarDecl(
         val type: TypeRef,
         val name: String,
-        val span: Span? = null
-    )
+        override val span: Span? = null
+    ) : Stmt
+
+    data class If(
+        val cond: Expr,
+        val thenBranch: Block,
+        val elseBranch: Stmt?,
+        override val span: Span? = null
+    ) : Stmt
+
+    data class While(
+        val cond: Expr,
+        val body: Block,
+        override val span: Span? = null,
+    ) : Stmt
+
+    data class Return(
+        val expr: Expr,
+        override val span: Span? = null
+    ) : Stmt
+
+    data class Break(
+        override val span: Span? = null
+    ) : Stmt
+
+    data class Continue(
+        override val span: Span? = null
+    ) : Stmt
 }
