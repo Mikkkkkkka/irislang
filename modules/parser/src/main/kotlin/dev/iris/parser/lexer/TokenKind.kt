3517e8d4db971f37a10aaf7192b4a0cb039dad70
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-01-18 19:12:36 +0300

    lexer: update TokenKind.kt

--- modules/parser/src/main/kotlin/dev/iris/parser/lexer/TokenKind.kt
+++ modules/parser/src/main/kotlin/dev/iris/parser/lexer/TokenKind.kt
@@ -12,17 +12,20 @@ enum class TokenKind {
     KW_BREAK,           // выйти
     KW_PROCEDURE,       // процедура
     KW_FUNCTION,        // функция
-    KW_STRUCT,          // структура
+    KW_STRUCTURE,       // структура
     KW_NEW,             // новый
     KW_PRINT,           // печать
     KW_START, KW_END,   // нч кц
     KW_TRUE, KW_FALSE,  // правда ложь
+    KW_AND, KW_OR,      // и или
+    KW_NULL,            // нуль
 
-    PLUS, MINUS, STAR, SLASH, PERCENT, BANG,    // + - * / %
-    EQ, EQEQ, NEQ,                              // = == !=
-    LT, GT, GTEQ, LTEQ,                         // < > <= >=
-    DOT, COMMA, COLON, SEMICOLON,               // . , : ;
-    LPAREN, RPAREN,                             // ( )
+    PLUS, MINUS, STAR, SLASH, PERCENT,  // + - * / %
+    BANG, QUESTION,                     // ! ?
+    EQ, EQEQ, NEQ,                      // = == !=
+    LT, GT, GTEQ, LTEQ,                 // < > <= >=
+    DOT, COMMA, COLON, SEMICOLON,       // . , : ;
+    LPAREN, RPAREN,                     // ( )
 
     ERROR,              // (для ошибок)
     EOF,
